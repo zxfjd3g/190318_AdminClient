5247c4a21a643e33fc7048dc1498e98bb395f766
zxfjd3g <258147149@qq.com>
2019-07-14 15:01:41 +0800

    react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-/* 
+import React, {Component} from 'react'
+import {Button, message} from 'antd'
+import { BrowserRouter, HashRouter, Switch, Route} from "react-router-dom"
+
+import Login from './pages/login/login.jsx'
+import Admin from './pages/admin/admin.jsx'
+
+/*
 应用根组件
-*/
-import React, { Component } from 'react'
+ */
+class App extends Component {
 
+  handleClick = () => {
+    message.success('成功啦...');
+  }
 
-export default class App extends Component {
-
-  render () {
+  render() {
     return (
-      <div>App</div> 
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Admin} />
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
+
+export default App

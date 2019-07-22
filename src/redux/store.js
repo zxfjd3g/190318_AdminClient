56dea3648d1225344d4fb3ca0ac8be7edd3ff45f
zxfjd3g <258147149@qq.com>
2019-07-22 15:21:11 +0800

    redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,11 @@ redux最核心的管理对象: store
 */
 import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 import reducer from './reducer'
 
 // 根据指定的reducer函数, 产生一个store对象
 // store对象内部管理新状态数据, 状态数据的初始值为第一次调用reducer()的返回值
 // 应用异步中间件
-const store = createStore(reducer, applyMiddleware(thunk))
-export default store
+export default createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
